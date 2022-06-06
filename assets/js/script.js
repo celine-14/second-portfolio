d7b191b3c7cd67a9e7c2d69c863f8e28b72c30de
celine-14 <leong_celine@outlook.com>
2022-06-06 13:39:40 +0000

    Add questions and answers data

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+/* Questions and Answers Data */
+
+const questions = {
+    medieval: [
+        {
+            text: "A type of musical instrument that is between a harp and a lyre.",
+            answer: "a",
+            a: "Psaltery",
+            b: "Adufe",
+            c: "Rebec",
+            d: "Flageolet"
+        }
+    ],
+    renaissance: [
+        {
+            text: "A type of liturgical music of the Roman Catholic Church used to accompany the text of the mass and the canonical hours, or divine office.",
+            a: "Sea Shanties",
+            b: "Oratorio",
+            c: "Freestyle",
+            d: "Gregorian Chant",
+            answer: "d"
+        }
+    ],
+    baroque: [
+        {
+            text: "Which Baroque musician was the first conductor to die from a baton?",
+            a: "George Frideric Handel",
+            b: "Johann Sebastian Bach",
+            c: "Jean-Baptiste Lully",
+            d: "Walter White",
+            answer: "c",
+        }
+    ],
+    classical: [
+        {
+            text: "A musical term for when all the voices or instruments perform together.",
+            answer: "a",
+            a: "tutti",
+            b: "alle",
+            c: "everybody",
+            d: "frutti"
+        },
+    ],
+    romantic: [
+        {
+            text: 'Which 19th-century composer and violinist was known as "The Devil\'s Violinist"?',
+            a: "Vivaldi",
+            b: "Paganini",
+            c: "Fettuccine",
+            d: "Salieri",
+            answer: "b"
+        }
+    ],
+    twentieth: [
+        {
+            text: "Known for his orchestral compositions and his musicals, which composer died of a brain tumor at the age of 38?",
+            answer: "a",
+            a: "George Gershwin",
+            b: "Ludwig van Beethoven",
+            c: "Arnold Schwazernegger",
+            d: "Dmitri Shostakovich"
+        }
+    ]
+
+};
