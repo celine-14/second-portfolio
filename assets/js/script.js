d11dfd924fe1ab0d21c3733d098408b998b6d138
celine-14 <leong_celine@outlook.com>
2022-06-06 13:41:33 +0000

    Add event listeners and remove data-type n answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,3 +63,21 @@ const questions = {
     ]
 
 };
+
+// Wait for the DOM to finish downloading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer(this.getAttribute("id"));
+            } else {
+                let gameType = this.getAttribute("data-type");
+                showQuestion(gameType);
+            }
+        })
+    }
+});
