0d839273c248fbcf9e5d369ad92488dbbfa6c6b5
celine-14 <leong_celine@outlook.com>
2022-06-07 12:01:53 +0000

    Delete attempted questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,54 @@
 /* Questions and Answers Data */
 
 const questions = {
-    medieval: [
-        {
-            text: "A type of musical instrument that is between a harp and a lyre.",
-            answer: "a",
-            a: "Psaltery",
-            b: "Adufe",
-            c: "Rebec",
-            d: "Flageolet"
-        }
-    ],
-    renaissance: [
-        {
-            text: "A type of liturgical music of the Roman Catholic Church used to accompany the text of the mass and the canonical hours, or divine office.",
-            a: "Sea Shanties",
-            b: "Oratorio",
-            c: "Freestyle",
-            d: "Gregorian Chant",
-            answer: "d"
-        }
-    ],
-    baroque: [
-        {
-            text: "Which Baroque musician was the first conductor to die from a baton?",
-            a: "George Frideric Handel",
-            b: "Johann Sebastian Bach",
-            c: "Jean-Baptiste Lully",
-            d: "Walter White",
-            answer: "c",
-        }
-    ],
-    classical: [
-        {
-            text: "A musical term for when all the voices or instruments perform together.",
-            answer: "a",
-            a: "tutti",
-            b: "alle",
-            c: "everybody",
-            d: "frutti"
-        },
-    ],
-    romantic: [
-        {
-            text: 'Which 19th-century composer and violinist was known as "The Devil\'s Violinist"?',
-            a: "Vivaldi",
-            b: "Paganini",
-            c: "Fettuccine",
-            d: "Salieri",
-            answer: "b"
-        }
-    ],
-    twentieth: [
-        {
-            text: "Known for his orchestral compositions and his musicals, which composer died of a brain tumor at the age of 38?",
-            answer: "a",
-            a: "George Gershwin",
-            b: "Ludwig van Beethoven",
-            c: "Arnold Schwazernegger",
-            d: "Dmitri Shostakovich"
-        }
-    ]
+    medieval: [{
+        text: "A type of musical instrument that is between a harp and a lyre.",
+        answer: "a",
+        a: "Psaltery",
+        b: "Adufe",
+        c: "Rebec",
+        d: "Flageolet"
+    }],
+    renaissance: [{
+        text: "A type of liturgical music of the Roman Catholic Church used to accompany the text of the mass and the canonical hours, or divine office.",
+        a: "Sea Shanties",
+        b: "Oratorio",
+        c: "Freestyle",
+        d: "Gregorian Chant",
+        answer: "d"
+    }],
+    baroque: [{
+        text: "Which Baroque musician was the first conductor to die from a baton?",
+        a: "George Frideric Handel",
+        b: "Johann Sebastian Bach",
+        c: "Jean-Baptiste Lully",
+        d: "Walter White",
+        answer: "c",
+    }],
+    classical: [{
+        text: "A musical term for when all the voices or instruments perform together.",
+        answer: "a",
+        a: "tutti",
+        b: "alle",
+        c: "everybody",
+        d: "frutti"
+    }, ],
+    romantic: [{
+        text: 'Which 19th-century composer and violinist was known as "The Devil\'s Violinist"?',
+        a: "Vivaldi",
+        b: "Paganini",
+        c: "Fettuccine",
+        d: "Salieri",
+        answer: "b"
+    }],
+    twentieth: [{
+        text: "Known for his orchestral compositions and his musicals, which composer died of a brain tumor at the age of 38?",
+        answer: "a",
+        a: "George Gershwin",
+        b: "Ludwig van Beethoven",
+        c: "Arnold Schwazernegger",
+        d: "Dmitri Shostakovich"
+    }]
 
 };
 
@@ -77,6 +65,7 @@ document.addEventListener("DOMContentLoaded", function () {
             } else {
                 let gameType = this.getAttribute("data-type");
                 showQuestion(gameType);
+                delete questions[gameType]; //delete attempeted questions
             }
         })
     }
@@ -86,26 +75,26 @@ let question;
 
 function showQuestion(gameType) {
 
-    question = questions[gameType][0];
-
-    document.getElementById("question-area").textContent = question.text;
-    document.getElementById("a").textContent = question.a;
-    document.getElementById("b").textContent = question.b;
-    document.getElementById("c").textContent = question.c;
-    document.getElementById("d").textContent = question.d;
-
+    if (question = questions[gameType][0]) {
+        document.getElementById("question-area").textContent = question.text;
+        document.getElementById("a").textContent = question.a;
+        document.getElementById("b").textContent = question.b;
+        document.getElementById("c").textContent = question.c;
+        document.getElementById("d").textContent = question.d;
+    } 
+    else if (gameType != questions[gameType]) {
+        alert("You have selected this category. Pick another!")
+    }
 }
 
 function checkAnswer(option) {
     if (option === question.answer) {
         alert("You got it right! Select a new category to continue playing.");
         incrementScore();
-        removeAttemptedQuestion();
-        
+
     } else {
         alert(`Awww... you answered ${question[option]}. The correct answer was ${question[question.answer]}. Select a new category to continue playing!`);
         incrementWrongAnswer();
-        removeAttemptedQuestion();
     }
 }
 
@@ -119,17 +108,4 @@ function incrementWrongAnswer() {
     document.getElementById("incorrect").innerText = ++incorrect;
 }
 
-function removeAttemptedQuestion() {
-    let gameType = button.getAttribute("data-type")
-    if (attempt === gameType) {
-        alert("You have already picked this category!")
-        delete questions[attempt];
-    }
-}
-    
-
- 
-//delete questions.medieval
 console.log(questions)
-
-
